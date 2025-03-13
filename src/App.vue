60c1b544cb83ca65a9fbf6944d5328796265eb0a
rafih268 <rafi.h2682002@gmail.com>
2025-03-13 14:57:02 +0000

    feat: add sidebar component with new note button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import Sidebar from './components/Sidebar.vue'
 
 </script>
 
 <template>
-  <div>Initial State of Project</div>
+  <div class="bg-gradient-to-br from-yellow-50 to-yellow-200 text-gray-900 min-h-screen flex items-stretch justify-start">
+    <Sidebar/>
+    <main class="flex-1">
+      Title + Content
+    </main>
+  </div>
 </template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,7 @@
+<template>
+  <aside class="w-[156px] min-h-screen flex justify-start">
+    <div class="p-4">
+      <button class="button w-full">Note +</button>
+    </div>
+  </aside>
+</template>
