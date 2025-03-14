7ab59303ad95ef634fe3018d22fea64b80232ff7
rafih268 <rafi.h2682002@gmail.com>
2025-03-14 15:33:52 +0000

    feat: add list element in sidebar where notes can be stored and viewed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,14 @@
 <template>
-  <aside class="w-[156px] min-h-screen flex justify-start">
+  <aside class="w-[256px] min-h-screen">
     <div class="p-4">
       <button class="button w-full">Note +</button>
     </div>
+
+    <ul>
+      <li class="p-4 border-b border-gray-600 hover:bg-yellow-300 rounded cursor-pointer flex justify-between items-center gap-4">
+        <span>Untitled</span>
+        <button class="text-red-500 cursor-pointer">Delete</button>
+      </li>
+    </ul>
   </aside>
 </template>
