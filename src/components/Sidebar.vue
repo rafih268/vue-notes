1d03d489761ffd701be54c1059e411e291ffc71d
rafih268 <rafi.h2682002@gmail.com>
2025-03-14 23:45:51 +0000

    style(sidebar): update styling for better UI

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,11 @@
 <template>
-  <aside class="w-[256px] min-h-screen">
+  <aside class="w-[256px] min-h-screen border-r border-gray-600">
     <div class="p-4">
       <button class="button w-full">Note +</button>
     </div>
 
     <ul>
-      <li class="p-4 border-b border-gray-600 hover:bg-yellow-300 rounded cursor-pointer flex justify-between items-center gap-4">
+      <li class="p-4 border-b border-gray-600 hover:bg-yellow-300 cursor-pointer flex justify-between items-center gap-4">
         <span>Untitled</span>
         <button class="text-red-500 cursor-pointer">Delete</button>
       </li>
