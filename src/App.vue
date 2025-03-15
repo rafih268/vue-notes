cdffd7077b428ed477e47576925874d9cfb5db7e
rafih268 <rafi.h2682002@gmail.com>
2025-03-15 00:00:48 +0000

    feat(editor): add header area and text area for notes

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@ import Sidebar from './components/Sidebar.vue'
   <div class="bg-gradient-to-br from-yellow-50 to-yellow-200 text-gray-900 min-h-screen flex items-stretch justify-start">
     <Sidebar/>
     <main class="flex-1">
-      Title + Content
+      <div class="px-4 py-8 flex flex-col h-full">
+        <input type="text" class="block w-full text-3xl pb-2 font-bold border-b-2 border-gray-600 focus:border-yellow-200 outline-none transition-colors duration-200">
+        <textarea class="block w-full h-full mt-4 text-lg outline-none flex-1"></textarea>
+      </div>
     </main>
   </div>
 </template>
